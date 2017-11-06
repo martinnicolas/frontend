58aa917fce7596c5b63a42e3b923f09f89cc4b2b
martinnicolas <ttincho77@gmail.com>
2017-11-05 21:38:27 -0300

    Continuar con modularizacion en diferentes componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+export class Persona {
+  id: number;
+  dni: number;
+  apellido: string;
+  nombre: string;
+}
+
+const PERSONAS: Persona[] = [
+  {id: 1, dni: 31343209, apellido: "Moreno", nombre: "Martin" },
+  {id: 2, dni: 32343209, apellido: "Gonzalez", nombre: "Nicolas" },
+  {id: 3, dni: 33343209, apellido: "Perez", nombre: "Denis" },
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +20,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Aplicacion en Angularjs';
+  selectedPersona: Persona;  
+  personas = PERSONAS;
+  onSelect(persona: Persona): void {
+    this.selectedPersona = persona;
+  }
 }
