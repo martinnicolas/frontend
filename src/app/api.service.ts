1342501113974af33eae6cdda775ce7ac4f44a59
martinnicolas <ttincho77@gmail.com>
2018-09-30 19:55:56 -0300

    Modifico baseURL para usar backend_api en produccion

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,7 @@
 export abstract class Api{
 
     //protected baseURL = 'http://localhost:8084/backend'; // URL to web api Java
-    protected baseURL = 'http://localhost:3000'; // URL to web api Rails
+    //protected baseURL = 'http://localhost:3000'; // URL to web api Rails
+    protected baseURL = 'http://personasbackend.herokuapp.com'; // URL to web api Rails
 
 }
