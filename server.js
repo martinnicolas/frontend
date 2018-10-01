a99dc12819630d851bc7fee5c357b1bb1b0ca487
martinnicolas <ttincho77@gmail.com>
2018-09-30 21:31:57 -0300

    Cambios para produccion

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/personasfrontend'));
+app.use(express.static('/dist/frontend'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('/dist/personasfrontend/index.html'));
+res.sendFile(path.join('/dist/frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
